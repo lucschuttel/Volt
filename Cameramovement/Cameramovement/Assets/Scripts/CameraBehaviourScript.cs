86525c6947f44289dca0a77a908cf0f6422fe9f1
Luc Schuttel <luc.schuttel@hva.nl>
2014-10-13 14:26:25 +0200

    Added Camera and Collision
    Beta

---
+++ Cameramovement/Cameramovement/Assets/Scripts/CameraBehaviourScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviourScript : MonoBehaviour {
+	
+	public GameObject target;					//the target object
+	private float speedMod = 10.0f;				//a speed modifier
+	private Vector3 point;						//the coord to the point where the camera looks at
+	private bool moving = false;
+	
+	void Start () {
+		moving = false;
+	}
+	
+	void Update () {
+		point = target.transform.position;		//get target's coords
+		transform.LookAt(point);				//makes the camera look to it
+
+		if (Input.GetKey ("a") || Input.GetKey ("s") || Input.GetKey ("d") || Input.GetKey ("w")) {
+			moving = true;
+		} else {
+			moving = false;
+		}
+
+		Rotate ();
+	}
+
+	void Rotate () {							//makes the camera rotate around "point" coords, rotating around its Y axis, 25 degrees per second times the speed modifier
+		if (Input.GetKey ("left") && moving == false) {
+			transform.RotateAround (point, new Vector3 (0.0f, 1.0f, 0.0f), 25 * Time.deltaTime * speedMod);
+		} else if (Input.GetKey ("right") && moving == false) {
+			transform.RotateAround (point, new Vector3 (0.0f, 1.0f, 0.0f), -25 * Time.deltaTime * speedMod);
+		}
+	}
+}
---
+++ Cameramovement/Cameramovement/Assets/Scripts/PlayerBehaviourScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviourScript : MonoBehaviour {
+
+	public bool moving = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movement();
+	}
+
+	void Movement (){
+		if (Input.GetKey("d")) {
+			transform.Translate (Vector3.right * 7f * Time.deltaTime);
+		}
+
+		if (Input.GetKey("a")) {
+			transform.Translate (Vector3.left * 7f * Time.deltaTime);
+		}
+
+		if (Input.GetKey("w")) {
+			transform.Translate (Vector3.forward * 7f * Time.deltaTime);
+		}
+
+		if (Input.GetKey("s")) {
+			transform.Translate (Vector3.back * 7f * Time.deltaTime);
+		}
+
+		//transform.eulerAngles.Set(0f,0f,0f);
+	}
+}
