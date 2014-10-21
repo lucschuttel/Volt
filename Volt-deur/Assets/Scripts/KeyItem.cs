1369443f15a99a74c434d7b44e83c809d84694d4
Gabriella de Wit <gabriella.de.wit@hva.nl>
2014-10-21 14:11:37 +0200

    deur
    klaar

---
+++ Volt-deur/Assets/Scripts/KeyItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyItem : MonoBehaviour {
+
+void OnTriggerEnter(Collider collider){
+		if (collider.gameObject.name == "Player"){
+			GameVariables.keyCount+=1;
+			Destroy (gameObject);
+	}
+}
+}
