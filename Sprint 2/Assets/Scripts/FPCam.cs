df311cf7e77dd8bd7246bab3e7a4ae33e6c28ca3
Rytje <Ryno.Tseng@hva.nl>
2014-11-19 19:54:56 +0100

    First Person Camera script toegevoegd

---
+++ Sprint 2/Assets/Scripts/FPCam.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class FPCam : MonoBehaviour {
+	private Transform transPlayer;
+	private Transform transCamera;
+	private Component hetScript;
+	private GameObject deCamera;
+	private bool yPressed;
+	public Vector3 posCamera;
+	// Use this for initialization
+	void Start () {
+		transPlayer = GameObject.FindGameObjectWithTag ("Player").transform;
+		transCamera = GameObject.FindGameObjectWithTag ("MainCamera").transform;
+		deCamera = GameObject.FindGameObjectWithTag ("MainCamera");
+		posCamera = new Vector3(0, 1.5f, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transCamera.position = transPlayer.position + posCamera;
+		transCamera.rotation = transPlayer.rotation;
+
+		yPressed = Input.GetKey(KeyCode.Y);
+
+		if (yPressed && deCamera.GetComponent<FPCam> ().enabled == true)
+		{
+			deCamera.GetComponent<FPCam> ().enabled = false;
+			deCamera.GetComponent<ThirdPersonCamera> ().enabled = true;
+		}
+
+		/*if (yPressed && deCamera.GetComponent<FPCam> ().enabled == false)
+		{
+			deCamera.GetComponent<FPCam> ().enabled = true;
+		}*/
+	}
+}
