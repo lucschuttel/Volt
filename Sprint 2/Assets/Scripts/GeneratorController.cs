b97ee3d425ef5ef99877c185a6e17266d5b2b9f7
Gabriella de Wit <gabriella.de.wit@hva.nl>
2014-11-19 18:08:51 +0100

    Level sprint 2
    Iedereen hier zijn eigen objects etc in zetten, in de Sprint2 scene

---
+++ Sprint 2/Assets/Scripts/GeneratorController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneratorController : MonoBehaviour {
+
+	GameObject player, camera;
+	Vector3 playerPos, cameraPos;
+	int range, angle;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		camera = GameObject.FindGameObjectWithTag ("MainCamera");
+		range = 3;
+		angle = 80;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerPos = player.transform.position;
+		cameraPos = camera.transform.position;
+
+		if(Vector3.Distance(playerPos, this.transform.position) < range)
+		{
+			if (Input.GetKey ("t")) {
+					Application.LoadLevel("LevelDone");
+			}
+		}
+	}
+}
+
+
