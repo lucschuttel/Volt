f3414c95e5309eac172bfbc6da497e3f297d1323
Joris van Willigen <joris.van.willigen@hva.nl>
2014-11-19 23:19:17 +0100

    Zoom functie
    moet nog worden toegevoegd aan de branch ( zoom uit met "C")

---
+++ Volt/UnityThirdPersonTutorial-master/UnityThirdPersonTutorial-master/Assets/Scripts/KeyDoor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyDoor : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider){
+		if (collider.gameObject.name == "Beta" && GameVariables.keyCount>0){
+			GameVariables.keyCount--;
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Volt/UnityThirdPersonTutorial-master/UnityThirdPersonTutorial-master/Assets/Scripts/KeyItem.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyItem : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider){
+		if (collider.gameObject.name == "Beta"){
+			GameVariables.keyCount+=2;
+			Destroy (gameObject);
+		}
+	}
+}
