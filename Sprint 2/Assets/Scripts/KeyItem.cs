b7213c685d7f302e620133ca1629163a2f7aa37b
Gabriella de Wit <gabriella.de.wit@hva.nl>
2015-01-08 20:05:24 +0100

    Revert "Sorry"
    This reverts commit 4fa7747c001fda5ed825999de938087a9a425657.

---
+++ Sprint 2/Assets/Scripts/KeyItem.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections;
+
+public class KeyItem : MonoBehaviour {
+
+
+	void OnTriggerEnter(Collider collider){
+		if (collider.gameObject.name == "Player"){
+			GameVariables.keyCount+=1;
+			Destroy (gameObject);
+		}
+	}
+}
