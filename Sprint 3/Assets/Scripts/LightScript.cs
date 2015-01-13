d5f95ffbf5a33c30eff2be3126c23d72b33877f9
Joris van Willigen <joris.van.willigen@hva.nl>
2015-01-13 17:04:21 +0100

    Lightscript
    map renamed naar sprint 3
    Lightscript aangemaakt,
    Lightscript toegepast in level 1 de andere volgen als deze af zijn.

---
+++ Sprint 3/Assets/Scripts/LightScript.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightScript : MonoBehaviour {
+
+	GameObject player;
+	Vector3 playerPos;
+	int range;
+	bool pluggedIn;
+	private Light[] lights;
+	public GameObject lightParent;
+	public GameObject targetObject;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		range = 3;
+		pluggedIn = false;
+
+		lights = lightParent.GetComponentsInChildren<Light> (true);
+		foreach (Light light in lights) 
+		{
+			light.enabled = false;
+		}
+		targetObject.active = false;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerPos = player.transform.position;
+		
+		if(Vector3.Distance(playerPos, this.transform.position) < range)
+		{
+
+			if (Input.GetKeyDown ("t")/* && ( GameVariables.playerHasPower || pluggedIn)*/) {
+				pluggedIn = !pluggedIn;
+				SwitchLights();
+			}
+
+		}
+	}
+	
+	void SwitchLights () {
+		foreach (Light light in lights) 
+		{
+			light.enabled = !light.enabled;
+			/* GameVariables.playerHasPower = !GameVariables.playerHasPower; */
+		}
+		targetObject.active = !targetObject.active;
+	}
+
+}
