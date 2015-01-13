45874c27a2c222c113b6be3ce2539c601807a7b0
Gabriella de Wit <gabriella.de.wit@hva.nl>
2015-01-14 00:21:52 +0100

    Lightswitch en player in level 2
    Nieuwe lightswitch en speler, verder gekomen met level 2

---
+++ Level 2/Assets/Scripts/ElevatorDoor.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorDoor : MonoBehaviour {
+
+	private GameObject liftDoorLeft, liftDoorRight, player, ropeEnd;
+	private float doorstartright, doorstartleft, range;
+	private bool doorOpen;
+
+	void Start () {
+		doorOpen = false;
+		liftDoorLeft = GameObject.FindGameObjectWithTag ("Liftdoor_Left");
+		doorstartleft = liftDoorLeft.transform.position.z;
+		liftDoorRight = GameObject.FindGameObjectWithTag ("Liftdoor_Right");
+		doorstartright = liftDoorRight.transform.position.z;
+		player = GameObject.FindGameObjectWithTag ("Player");
+		range = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("t") && Vector3.Distance (player.transform.position, this.transform.position) < range) {
+			doorOpen = !doorOpen;
+			PlugIn();
+		}
+
+		if (doorOpen) {
+			if (liftDoorLeft.transform.position.z >= doorstartleft + 1f)
+				liftDoorLeft.transform.Translate (0, 0, 0);
+			else 
+				liftDoorLeft.transform.Translate(0,0,0.1f);
+			
+			if (liftDoorRight.transform.position.z <= doorstartright - 1)
+				liftDoorRight.transform.Translate (0, 0, 0);
+			else 
+				liftDoorRight.transform.Translate(0,0,-0.1f);
+		}
+	}
+
+	void PlugIn(){
+		ropeEnd = GameObject.FindGameObjectWithTag ("RopeEnd");
+		ropeEnd.transform.position = this.transform.position;
+		//GameVariables.pluggedIn = true;
+	}
+}
