956f4c1e2f7908279454bfd2b7d4156c9ce4d68d
Gabriella de Wit <gabriella.de.wit@hva.nl>
2015-01-14 00:14:26 +0100

    Assets geordend

---
+++ Level 2/Assets/Scripts/LightWall.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightWall : MonoBehaviour {
+	public Light Light;
+
+
+	void OnTriggerEnter(Collider collider){
+		if (collider.gameObject.name == "Player" && Light.enabled == true){//&& GameVariables.light > 0) {
+			Destroy (gameObject);
+		}
+	}
+}
