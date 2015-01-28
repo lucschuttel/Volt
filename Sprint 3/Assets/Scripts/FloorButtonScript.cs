81bdc7a0714f57fde02301bd47572689a2b216a4
Nick Kortleve <op.genetics@gmail.com>
2015-01-28 22:57:27 +0100

    FloorButton

---
+++ Sprint 3/Assets/Scripts/FloorButtonScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorButtonScript : MonoBehaviour {
+
+	public Material material;
+	
+	private bool turnedOn;
+	private GameObject floorButton;
+
+
+	void Start () {
+		turnedOn = false;
+		floorButton = transform.Find("polySurface2").gameObject;
+	}
+
+	void OnTriggerEnter(Collider collider){
+		if (collider.gameObject.name == "box") {
+			    SetButtonStatus(true);
+		}
+	}
+
+	void OnTriggerExit(Collider collider){
+		if (collider.gameObject.name == "box") {
+			SetButtonStatus(false);
+		}
+	}
+
+	void SetButtonStatus(bool status){
+		if(status) {
+			turnedOn = true;
+			//material.SetColor ("_SpecColor", Color.green);
+			floorButton.renderer.material.color = Color.green;
+
+		} else {
+			turnedOn = false;
+			floorButton.renderer.material.color = Color.red;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//SetButtonStatus (false);
+	}
+}
